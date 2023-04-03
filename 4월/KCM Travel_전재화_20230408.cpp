af43def591348c1610ee72fdbe8a49049a0c869f
unknown <kuku9820@o365.yonsei.ac.kr>
2023-04-03 10:16:35 +0900

    4월 8일 3문제 upload

---
+++ 4월/KCM Travel_전재화_20230408.cpp
@@ -0,0 +1,108 @@
+﻿/*
+*	다익스트라 + dp를 이용한 풀이
+*	비슷하게 발상을 했으나, 데이터 구조를 어떻게 설정해야할지 몰라 답지를 참고했음
+* 
+*	시간 단축할 때 사용했던 기법에 대해서 토의해보는게 좋을 듯 함 (https://conkjh032.tistory.com/356)
+*/
+#include <iostream>
+#include <queue>
+#include <climits>
+#include <vector>
+using namespace std;
+
+struct EDGE
+{
+	int to, cost, time;
+};
+
+struct DATA
+{
+	int node, cost, time;
+	bool operator<(const DATA& a) const
+	{
+		return time > a.time;
+	}
+};
+
+int T, N, M, K, ans;
+int dp[101][10001];
+vector<EDGE> edge[101];
+
+void init(void)
+{
+	for (int i = 1; i <= N; i++)
+	{
+		edge[i].clear();
+		for (int j = 0; j <= M; j++)
+			dp[i][j] = INT_MAX;
+	}
+}
+
+void input(void)
+{
+	int u, v, c, d;
+	for (int i = 0; i < K; i++)
+	{
+		cin >> u >> v >> c >> d;
+		edge[u].push_back({ v,c,d });
+	}
+}
+
+void dij(void)
+{
+	priority_queue<DATA> pq;
+	pq.push({ 1,0,0 });
+	dp[1][0] = 0;
+
+	int nnode, ntime, ncost;
+	while (!pq.empty())
+	{
+		DATA now = pq.top();
+		pq.pop();
+
+		if (dp[now.node][now.cost] < now.time)
+			continue;
+
+		for (int i = 0; i < edge[now.node].size(); i++)
+		{
+			nnode = edge[now.node][i].to;
+			ncost = now.cost + edge[now.node][i].cost;
+			ntime = now.time + edge[now.node][i].time;
+						
+			if (ncost <= M && ntime < dp[nnode][ncost]) //ncost <= M 유의! --> out of bound 예방
+			{
+				pq.push({ nnode, ncost, ntime });
+				for (int j = ncost; j <= M; j++)
+				{
+					if (dp[nnode][j] < ntime) //시간 단축법 --> break시에 시간이 많이 줄어드는 정확한 이유는 모르겠음
+						break;
+					dp[nnode][j] = ntime;
+				}
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> T;
+	for (int tc = 0; tc < T; tc++)
+	{
+		cin >> N >> M >> K;
+		init();
+		input();
+		dij();
+		
+		ans = INT_MAX;
+		for (int i = 0; i <= M; i++)
+			ans = min(ans, dp[N][i]);
+
+		if (ans == INT_MAX)
+			cout << "Poor KCM" << '\n';
+		else
+			cout << ans << '\n';
+	}
+}
---
+++ 4월/거울설치_전재화_20230408.cpp
@@ -0,0 +1,95 @@
+﻿/*
+*	거울의 상하좌우 직진성을 이용한 다익스트라 문제
+*	벽에 있을 때도(53번째 줄) break해줘야 하는 것에 유의!!!
+*/
+#include <iostream>
+#include <queue>
+#include <climits>
+using namespace std;
+
+#define DOOR '#'
+#define EMPTY '.'
+#define MIRROR '!'
+#define WALL '*'
+
+struct DATA
+{
+	int y, x, num;
+	bool operator<(const DATA& a) const
+	{
+		return num > a.num;
+	}
+};
+
+int N;
+int dy[4] = { -1,+1,0,0 };
+int dx[4] = { 0,0,-1,+1 };
+char map[50][50];
+int cost[50][50];
+priority_queue<DATA> pq;
+
+inline bool isrange(int y, int x)
+{
+	return y >= 0 && y < N && x >= 0 && x < N;
+}
+
+void dij(void)
+{
+	while (!pq.empty())
+	{
+		DATA now = pq.top();
+		pq.pop();
+
+		if (cost[now.y][now.x] < now.num)
+			continue;
+
+		for (int dir = 0; dir < 4; dir++)
+		{
+			for (int i = 1; i < N; i++)
+			{
+				int ny = now.y + dy[dir] * i;
+				int nx = now.x + dx[dir] * i;
+				
+				if (!isrange(ny, nx) || map[ny][nx] == WALL)
+					break;
+
+				int nnum;
+				if ((map[ny][nx] == MIRROR || map[ny][nx] == DOOR) && (nnum = now.num + 1) < cost[ny][nx])
+				{
+					cost[ny][nx] = nnum;
+					pq.push({ ny,nx,nnum });
+				}
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	cin >> N;
+	bool door = false;
+	int yy, xx;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> map[i][j];
+			cost[i][j] = INT_MAX;
+			if (map[i][j] == DOOR && !door)
+			{
+				pq.push({ i,j,0 });
+				cost[i][j] = 0;
+				door = true;
+			}
+			else if (map[i][j] == DOOR && door)
+			{
+				yy = i;
+				xx = j;
+			}
+		}
+	}
+
+	dij();
+
+	cout << cost[yy][xx] - 1 << '\n';
+}
